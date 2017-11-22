d8abfa5c08944c97741b06d8df664621730754be
Pietro Cerutti <gahr@gahr.ch>
2017-11-22 17:06:37 +0000

    Revert "Do not use {0}, use mutt_buffer_init explicitely"
    This reverts commit 027f95c20a48da21a28ee7055ab0e26b32c6cace.

--- imap/imap.c
+++ imap/imap.c
@@ -1212,12 +1212,10 @@ int imap_exec_msgset(struct ImapData *idata, const char *pre, const char *post,
 {
   struct Header **hdrs = NULL;
   short oldsort;
-  struct Buffer cmd;
+  struct Buffer cmd = {0};
   int pos;
   int rc;
   int count = 0;
-
-  mutt_buffer_init(&cmd);
 
   /* We make a copy of the headers just in case resorting doesn't give
    exactly the original order (duplicate messages?), because other parts of
@@ -1931,8 +1929,7 @@ int imap_fast_trash(struct Context *ctx, char *dest)
     mutt_str_strfcpy(mbox, "INBOX", sizeof(mbox));
   imap_munge_mbox_name(idata, mmbox, sizeof(mmbox), mbox);
 
-  struct Buffer sync_cmd;
-  mutt_buffer_init(&sync_cmd);
+  struct Buffer sync_cmd = {0};
   for (int i = 0; i < ctx->msgcount; i++)
   {
     if (ctx->hdrs[i]->active && ctx->hdrs[i]->changed &&
@@ -2711,8 +2708,7 @@ static int imap_commit_message_tags(struct Context *ctx, struct Header *h, char 
   /* Remove old custom flags */
   if (HEADER_DATA(h)->flags_remote)
   {
-    struct Buffer cmd;
-    mutt_buffer_init(&cmd);
+    struct Buffer cmd = {0};
     mutt_buffer_addstr(&cmd, "UID STORE ");
     mutt_buffer_addstr(&cmd, uid);
     mutt_buffer_addstr(&cmd, " -FLAGS.SILENT (");
@@ -2733,8 +2729,7 @@ static int imap_commit_message_tags(struct Context *ctx, struct Header *h, char 
   /* Add new custom flags */
   if (tags)
   {
-    struct Buffer cmd;
-    mutt_buffer_init(&cmd);
+    struct Buffer cmd = {0};
     mutt_buffer_addstr(&cmd, "UID STORE ");
     mutt_buffer_addstr(&cmd, uid);
     mutt_buffer_addstr(&cmd, " +FLAGS.SILENT (");
